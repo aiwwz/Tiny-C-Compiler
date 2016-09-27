52ec909a2cd2a76212a771b6dfcdaf51919976c9
王志军 <jxzh1023@163.com>
2016-09-27 15:16:11 +0800

    语法分析模块。

--- ToyL/ToyL.c
+++ ToyL/ToyL.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 //Token--单词的编码
 #define BEGIN 1
@@ -13,33 +15,45 @@
 #define OPEN 10
 #define CLOSE 11
 #define END 12
-#define EOF 13
+
 
 //判断读入的字符的类型
 #define is_end_of_input(ch) ((ch)== '\0')
 #define is_lc_letter(ch) ('a'<=(ch) && (ch)<='Z')
 #define is_uc_letter(ch) ('A'<=(ch) && (ch)<='Z')
 #define is_letter(ch) ('a'<=(ch) && (ch)<='z' || 'A'<=(ch) && (ch)<='Z')
-#define is_digit(ch) ( '0'<(ch) && (ch)<='9')
+#define is_digit(ch) ('0'<=(ch) && (ch)<='9')
 #define is_letter_or_digit(ch) (is_letter(ch) && is_digit(ch))
 #define is_operator(ch) ((ch)=='+' || (ch)=='-' || (ch)=='*')
 #define is_layout(ch) (!is_end_of_input(ch) && (ch)<=' ')  //格式符
 
-typedef struct {
-	int class;  //单词编码
-	char seman[];   //语义
-					//position_in_File pos;  //当前单词在程序文件中的位置
-}Token_type;
+typedef struct Token_Node * PtrToNode;
+typedef PtrToNode Token;
+typedef PtrToNode Position;
 
-typedef struct {
-	char * file_name;  //指向单词位置的文件指针
-	int line_number;  //单词在程序文本中的行号
-	int token_number; //单词在行中的序号
-}position_in_File;
+struct Token_Node {
+	int class;  //单词编码
+	char * seman;   //语义
+	Position  last;
+	Position  next;
+};
+
+char ch;  //定义全局字符变量
+FILE *fp;  //定义全局文件指针变量
+Position P;  //定义全局token指针
+
+Token init_token() {
+	Token T;
+	T = malloc(sizeof(struct Token_Node));
+	T->next = NULL;
+	T->last = NULL;
+	P = T;
+	return T;
+}
 
 //两种类型的读字符函数：过滤格式符
 void next_Char() {
-	ch = prog_file[++fp];
+	ch = fgetc(fp);
 }
 void next_avail_char() {
 	next_Char();
@@ -49,21 +63,28 @@ void next_avail_char() {
 }
 
 //读进常数并将常数从字符串翻译到数字
-void recognize_number(ch) {
+void recognize_number() {
 	int N = 0;
 	while (is_digit(ch)) {
 		N = N * 10 + (ch)-'0';
 		next_Char();
 	}
-	Token.class = NUMB;
-	Token.seman = N;
-	back();
+	Position Tmp;
+	Tmp = malloc(sizeof(struct Token_Node));
+	Tmp->class = NUMB;
+	Tmp->seman = malloc(sizeof(char) + 1);
+	Tmp->seman[0] = N;  //将翻译后的数字直接存入字符数组中，字符数组的第一个元素即为ASCII码为N对应的字符
+	Tmp->seman[1] = '\0';
+	P->next = Tmp;
+	Tmp->last = P;
+	P = Tmp;
+	//back();
 }
 
 //读进标识符
-void recognize_name(ch) {
+void recognize_name() {
 	char name[10];
-	np = 0;
+	int np = 0;
 	name[np++] = ch;
 	next_Char();
 	while (is_digit(ch) || is_letter(ch)) {
@@ -71,104 +92,173 @@ void recognize_name(ch) {
 		next_Char();
 	}
 	name[np] = '\0';
-	switch (ch) {
-	case "begin": Token.class = BEGIN; Token.seman = "begin"; break;
-	case "end": Token.class = END; Token.seman = "end"; break;
-	case "read": Token.class = READ; Token.seman = "read"; break;
-	case "write": Token.class = WRITE; Token.seman = "write";  break;
-	default: Token.class = IDEN; Token.seman = name; break;
+	if (strcmp(name, "begin") == 0) {
+		Position Tmp;
+		Tmp = malloc(sizeof(struct Token_Node));
+		Tmp->class = BEGIN;
+		Tmp->seman = "begin";
+		P->next = Tmp;
+		Tmp->last = P;
+		P = Tmp;
+		Tmp->next = NULL;
 	}
-	back();
+	else if (strcmp(name, "end") == 0) {
+		Position Tmp;
+		Tmp = malloc(sizeof(struct Token_Node));
+		Tmp->class = END;
+		Tmp->seman = "end";
+		P->next = Tmp;
+		Tmp->last = P;
+		P = Tmp;
+		Tmp->next = NULL;
+	}
+	else if (strcmp(name, "read") == 0) {
+		Position Tmp;
+		Tmp = malloc(sizeof(struct Token_Node));
+		Tmp->class = READ;
+		Tmp->seman = "read";
+		P->next = Tmp;
+		Tmp->last = P;
+		P = Tmp;
+		Tmp->next = NULL;
+	}
+	else if (strcmp(name, "write") == 0) {
+		Position Tmp;
+		Tmp = malloc(sizeof(struct Token_Node));
+		Tmp->class = WRITE;
+		Tmp->seman = "write";
+		P->next = Tmp;
+		Tmp->last = P;
+		P = Tmp;
+		Tmp->next = NULL;
+	}
+	else {
+		Position Tmp;
+		Tmp = malloc(sizeof(struct Token_Node));
+		Tmp->class = IDEN;
+		Tmp->seman = malloc(sizeof(char) * strlen(name));
+		strcpy(Tmp->seman, name);
+		P->next = Tmp;
+		Tmp->last = P;
+		P = Tmp;
+		Tmp->next = NULL;
+	}
+	//back();
 }
 
 //仅读一个单词的词法分析器
 void next_token(void) {
 	next_avail_char();
+	printf("1\n");
 	switch (ch) {
-	case '0': case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9': recognize_number(ch); break;
+	case '0': case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9': recognize_number(); break;
 
 	case 'a': case 'b': case 'c': case 'd': case 'e': case 'f': case 'g': case 'h': case 'i': case 'j': case 'k':
 	case 'l': case 'm': case 'n': case 'o': case 'p': case 'q': case 'r': case 's': case 't': case 'u': case 'v':
 	case 'w': case 'x': case 'y': case 'z':
 	case 'A': case 'B': case 'C': case 'D': case 'E': case 'F': case 'G': case 'H': case 'I': case 'J': case 'K':
 	case 'L': case 'M': case 'N': case 'O': case 'P': case 'Q': case 'R': case 'S': case 'T': case 'U': case 'V':
-	case 'W': case 'X': case 'Y': case 'Z': recognize_name(ch); break;
+	case 'W': case 'X': case 'Y': case 'Z': recognize_name(); break;
 
-	case '+': Token.class = PLUS; break;
-	case '*': Token.class = MULT; break;
-	case ':': next_char(); if (ch != '=') error();
-		Token.class = ASS;  break;
-	case ';': Token.class = SEMI; break;
-	case '(': Token.class = OPEN; break;
-	case ')': Token.class = CLOSE; break;
-	case '\0': Token.class = EOF;  break;
-	default: error();
+	case '+': {
+		Position Tmp;
+		Tmp = malloc(sizeof(struct Token_Node));
+		Tmp->class = PLUS;
+		Tmp->seman = "+";
+		P->next = Tmp;
+		Tmp->last = P;
+		P = Tmp;
+		Tmp->next = NULL; 
+		break;
 	}
-}
-
-//语法分析模块
-void next_token(Token_type tk) {
-	old_fp = fp;
-	/*  ...  */
-}
-void back_token() {
-	if (fp != old_fp)
-		fp = fp - 1;
-}
-void token(int CLASS) {
-	next_token(tk);
-	if (tk.class != CLASS)
-		error();
-}
-//表达式分析
-void  expr_parser() {
-	1000	next_token(tk);
-	if (tk.class != NUMB && tk.class != IDEN) error();
-	2000  next_token(tk);
-	if (tk.class == PLUS || tk.calss == MULT) goto 1000;
-	3000  back_token();
-	return;
-}
-//语法分析
-void prog_parser() {
-	token(BEGIN);
-	while (tk.class != END) {
-		next_token(tk);
-		switch (tk.class) {
-		case READ: token(OPEN); token(IDEN);   token(CLOSE); break;
-		case WRITE: token(OPEN); expr_parser(); token(CLOSE); break;
-		case IDEN: token(ASS);  expr_parser(); break;
-		default: error();
-		}
-		token(SEMI); //读语句的后继字符
+	case '*': {
+		Position Tmp;
+		Tmp = malloc(sizeof(struct Token_Node));
+		Tmp->class = MULT;
+		Tmp->seman = "*";
+		P->next = Tmp;
+		Tmp->last = P;
+		P = Tmp;
+		Tmp->next = NULL;
+		break;
 	}
-	token(EOF);
-	return;
-}
-
-//虚拟存储器:存放变量的值
-typedef struct {
-	char *name[10];
-	int val;
-}Memory[100];
-int fetch(char *id[]) {  //从Memory求得变量id的值，id为标识符
-	i = 0;
-	while (!strcmp(Memory[i].name, id) && i <= mp) ++i; //mp为全局变量，Memory[mp]表示最后一个元素
-	if (i > mp) error();
-	else return Memory[i].val;
-}
-void updata(char * id[], int n) {
-	i = 0;
-	while (!strcmp(Memory[i].name, id) && i <= mp) ++i;
-	if (i <= mp)
-		Memory[i].val = n;
-	else {
-		Memory[i].name = id;
-		Memory[mp].val = n;
+	case ':': {
+		next_Char(); 
+		if (ch != '=') break;
+		Position Tmp;
+		Tmp = malloc(sizeof(struct Token_Node));
+		Tmp->class = ASS;
+		Tmp->seman = ":=";
+		P->next = Tmp;
+		Tmp->last = P;
+		P = Tmp;
+		Tmp->next = NULL;
+		break;
+	}
+	case ';': {
+		Position Tmp;
+		Tmp = malloc(sizeof(struct Token_Node));
+		Tmp->class = SEMI;
+		Tmp->seman = ";";
+		P->next = Tmp;
+		Tmp->last = P;
+		P = Tmp;
+		Tmp->next = NULL;
+		break;
+	}
+	case '(': {
+		Position Tmp;
+		Tmp = malloc(sizeof(struct Token_Node));
+		Tmp->class = OPEN;
+		Tmp->seman = "(";
+		P->next = Tmp;
+		Tmp->last = P;
+		P = Tmp;
+		Tmp->next = NULL;
+		break;
+	}
+	case ')': {
+		Position Tmp;
+		Tmp = malloc(sizeof(struct Token_Node));
+		Tmp->class = CLOSE;
+		Tmp->seman = ")";
+		P->next = Tmp;
+		Tmp->last = P;
+		P = Tmp;
+		Tmp->next = NULL;
+		break;
+	}
+	case '\0': {
+		Position Tmp;
+		Tmp = malloc(sizeof(struct Token_Node));
+		Tmp->class = EOF;
+		Tmp->seman = "eof";
+		P->next = Tmp;
+		Tmp->last = P;
+		P = Tmp;
+		Tmp->next = NULL;
+		break;
+	}
+	case EOF: printf("eof"); break;
+	default: break;
 	}
 }
 
 
 int main() {
+	Token T;
+	T = init_token();
+	fp = fopen("C:\\1.txt", "r");
+
+	do {
+		next_token();
+	} while (ch != EOF);
+
+	P = T->next;
+	while (P != NULL) {
+		printf("%d,%s\n", P->class, P->seman);
+		P = P->next;
+	}
+
 	return 0;
 }
