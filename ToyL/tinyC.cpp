d1538f5f030dcf92beb3efb32520be505e96d068
王志军 <jxzh1023@163.com>
2018-01-07 19:30:34 +0800

    使用C++改写

---
+++ ToyL/tinyC.cpp
@@ -0,0 +1,15 @@
+/********************************
+* Tiny-C 编译器 
+* 版本：v1.0
+* 开发：WangZhijun
+* 时间：2018.01.07
+*********************************/
+
+#include <iostream>
+using std::cin; using std::cout; using std::endl;
+
+int main(int argc, char *argv[]) {
+    cout << "使用C++改写！" << endl;
+
+    return 0;
+}
